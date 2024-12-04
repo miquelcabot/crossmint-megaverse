a0c4c253787fc74e7426b27825f40fa8b05ec1a4
miquelcabot <miquel.cabot@gmail.com>
2024-12-04 09:54:46 +0100

    add figlet-rs dependency and generate ASCII art in main function

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use figlet_rs::FIGfont;
+
 #[tokio::main]
 async fn main() {
+    // Generate ASCII art
+    let standard_font = FIGfont::standard().unwrap();
+    let figure = standard_font
+        .convert("Megaverse")
+        .expect("Failed to generate ASCII art");
+    println!("{}", figure);
+
     println!("Hello, world!");
 }
