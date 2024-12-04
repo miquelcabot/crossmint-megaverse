f97c49a998b315d69a669758799eb95e0f589b34
miquelcabot <miquel.cabot@gmail.com>
2024-12-04 10:00:55 +0100

    add dialoguer dependency and implement interactive menu in main function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use dialoguer::{theme::ColorfulTheme, Input, Select};
 use figlet_rs::FIGfont;
 
 #[tokio::main]
@@ -9,5 +10,26 @@ async fn main() {
         .expect("Failed to generate ASCII art");
     println!("{}", figure);
 
-    println!("Hello, world!");
+    // Menu options
+    let options = vec!["Do a 🪐POLYanet cross", "Reset Megaverse", "Exit"];
+
+    // Loop to keep the menu active until "Exit" is selected
+    loop {
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Select an option:")
+            .items(&options)
+            .default(0)
+            .interact()
+            .unwrap();
+
+        match selection {
+            0 => todo!("Do a 🪐POLYanet cross"),
+            1 => todo!("Reset Megaverse"),
+            2 => {
+                println!("Exiting...");
+                break; // Exit the loop if "Exit" is selected
+            }
+            _ => println!("Invalid option"),
+        }
+    }
 }
